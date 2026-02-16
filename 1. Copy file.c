2dbd82f60a6560f28efda5d33463fc61fa806782
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-02-16 08:55:41 +0000

    Create 1. Copy file.c
    This C program performs a file copy operation from a source file to a destination file. It takes two arguments: the source file path and the destination file path. The program opens the source file in read-only mode and creates a new destination file with appropriate permissions. It reads the source file in chunks and writes the data to the destination file until the entire content is copied.
    
    Key features:
    
    Error handling for invalid arguments, source file not found, and failed write operations.
    
    The program utilizes system calls like open(), creat(), read(), and write() to handle file operations at a low level.
    
    Successfully closes both the source and destination files upon completion of the copy operation.
    
    Outputs success or error messages to the user.
    
    This is a basic implementation useful for understanding file handling and error management in C using system calls.

---
+++ 1. Copy file.c
@@ -0,0 +1,59 @@
+//Program to Copy a file 
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    int src_fd;
+    int dst_fd;
+    char buffer[100];
+    int n_read;
+    
+    //argument check
+    if(argc !=3 )
+    {
+        write(2, "Invalid arguments entered \n", 28);
+        _exit(1);
+    }
+    
+    //open source file
+    src_fd = open(argv[1], O_RDONLY);
+    
+    if(src_fd == -1)
+    {
+        write(2, "Error : Source file not found \n", 32);
+        _exit(1);
+    }
+
+    
+    //create file
+    dst_fd = creat(argv[2], 0644);
+    if(dst_fd == -1)
+    {
+        write(2, "ERROR : Unable to create destination file.\n", 33);
+        close(src_fd);
+        _exit(1);
+    }
+    
+    // copy data
+    while((n_read = read(src_fd, buffer, 100)) > 0)
+    {
+        if(write(dst_fd, buffer, n_read) != n_read)
+        {
+            write(2, "ERROR : Write operation failed \n", 33);
+            close(src_fd);
+            close(dst_fd);
+            _exit(1);
+        }
+    }
+    
+    //Final : Release final files
+    close(src_fd);
+    close(dst_fd);
+    
+    //success message
+    write(1, "Copy Operation Successful\n\n", 16);
+    
+    _exit(0);
+}
